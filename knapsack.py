2e8a814dcb1fe3e5f754d93e59412ed3002de393
matheus73 <matheuscoding@gmail.com>
2021-09-02 13:55:49 -0300

    :art: Criando algoritmo de ordenacao invertido baseado na classe Charge
    Co-authored-by: Pedrok99 <pedrohlima20@gmail.com>

--- knapsack.py
+++ knapsack.py
@@ -14,3 +14,12 @@ class Charge:
     
     def __str__(self):
         return f"Charge(Weight: {self.get_weight()} Value: {self.get_value()})"
+
+def bubbleSort_charge(charge_list):
+    for passnum in range(len(charge_list)-1,0,-1):
+        for i in range(passnum):
+            if charge_list[i].get_value_per_weight() < charge_list[i+1].get_value_per_weight():
+                temp = charge_list[i]
+                charge_list[i] = charge_list[i+1]
+                charge_list[i+1] = temp
+
