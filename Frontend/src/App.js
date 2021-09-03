aa2c9868cfbba7945b4906e01786a8a1fd327cff
matheus73 <matheuscoding@gmail.com>
2021-09-03 19:17:17 -0300

    :poop adicionando axios
    Co-authored-by: Pedrok99 <pedrohlima20@gmail.com>

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,102 +1,100 @@
 import Button from './components/Button';
 import Input from './components/Input';
 import List from './components/List';
+import axios from 'axios';
 import { useState} from 'react';
 import { useForm } from "react-hook-form";
 import './styles/global.css';
 
 function App() {
 
-  const [answers, setAnswers] = useState([]);
-  const { register, handleSubmit, reset, formState:{errors} } = useForm();
-  const {
-    register: register1,
-    formState: { errors: errors1 },
-    handleSubmit: handleSubmit1,
-  } = useForm();
-  
-  function onSubmit (data){
-    setAnswers([...answers,data]);
-    reset();
-  }
-  function sendButton(pesoMax){
-    const reqBody = {
-      pesoMax,
-      data:answers
+    const [answers, setAnswers] = useState([]);
+    const { register, handleSubmit, reset, formState:{errors} } = useForm();
+    const {
+        register: register1,
+        formState: { errors: errors1 },
+        handleSubmit: handleSubmit1,
+    } = useForm();
+
+    function onSubmit (data){
+        setAnswers([...answers,data]);
+        reset();
     }
-    console.log(reqBody);
+    function sendButton(pesoMax){
+        const reqBody = {
+            pesoMax,
+            data:answers
+        }
+        console.log(reqBody);
+        const headers = {'content-type': 'application/json'}     
+        axios.post('http://127.0.0.1:5000/charge/', reqBody, headers)     
+            .then(function (response) {       
+                console.log(response);     })
+            .catch(error => {       
+                console.log(error)   })
+    }
 
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(reqBody)
-  };
-  fetch('', requestOptions)
-      .then(response => response.json())
-      .then(data => console.log(data));
-  }
-
-  return (
-    <>
-      <div className="form">
-        <form key={0} onSubmit={handleSubmit(onSubmit)}>
-          <div className="inputField">
-            <Input 
-              register={register("nome", {
-                validate: value => !answers.some((item)=>item.nome===value) || 'Produto já existe!', 
-                required: 'Campo obrigatório!'
-              })} 
-              inputPlaceholder = "Nome do produto" 
-              inputTitle="Nome do produto"  
-              type="text" 
-              error={errors.nome}>
-            </Input>
-            <Input 
-              register={register("peso", {
+    return (
+        <>
+        <div className="form">
+            <form key={0} onSubmit={handleSubmit(onSubmit)}>
+                <div className="inputField">
+                    <Input 
+                    register={register("nome", {
+                    validate: value => !answers.some((item)=>item.nome===value) || 'Produto já existe!', 
+                    required: 'Campo obrigatório!'
+                    })} 
+                    inputPlaceholder = "Nome do produto" 
+                    inputTitle="Nome do produto"  
+                    type="text" 
+                    error={errors.nome}>
+                </Input>
+                <Input 
+                register={register("peso", {
                 validate: value => value > 0 || 'Peso inválido!', 
                 required: 'Campo obrigatório!'
-              })} 
-              inputPlaceholder = "Peso do produto" 
-              inputTitle="Peso do produto" 
-              type="number"
-              step="any"  
-              error={errors.peso}>
+                })} 
+                inputPlaceholder = "Peso do produto" 
+                inputTitle="Peso do produto" 
+                type="number"
+                step="any"  
+                error={errors.peso}>
             </Input>
             <Input 
-              register={register("valor", {
-                validate: value => value > 0 || 'Valor inválido!', 
-                required: 'Campo obrigatório!'
-              })} 
-              inputPlaceholder = "Valor do produto" 
-              inputTitle="Valor do produto" 
-              type="number"
-              step="any" 
-              error={errors.valor}>
-            </Input>
-          </div>
-          <Button title = 'Adicionar'/>
-        </form>
+            register={register("valor", {
+            validate: value => value > 0 || 'Valor inválido!', 
+            required: 'Campo obrigatório!'
+            })} 
+            inputPlaceholder = "Valor do produto" 
+            inputTitle="Valor do produto" 
+            type="number"
+            step="any" 
+            error={errors.valor}>
+        </Input>
+    </div>
+    <Button title = 'Adicionar'/>
+</form>
       </div>
       <List answers={answers} deleteCard={setAnswers}/>
       <form className="sendForm" onSubmit={handleSubmit1(sendButton)}>
-        <div className="sendFormContainer">
-        <Input 
+          <div className="sendFormContainer">
+              <Input 
               register={register1("pesoMax", {
-                validate: value => value > 0 || 'Valor inválido!'
+              validate: value => value > 0 || 'Valor inválido!'
               })} 
               inputPlaceholder = "Peso max" 
               inputTitle="Peso máximo suportado" 
               type="number"
               step="any" 
               error={errors1.pesoMax}>
-            </Input>
-        </div>
-      
-        <Button title = 'Calcular'/> 
+          </Input>
+      </div>
 
-      </form>
+      <Button title = 'Calcular'/> 
+
+  </form>
     </>
-  );
+    );
 }
 
 export default App;
