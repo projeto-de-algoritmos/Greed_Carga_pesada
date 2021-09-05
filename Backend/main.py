f9e9f466a9d76d03de60dc9cf416b5a611d94e17
matheus73 <matheuscoding@gmail.com>
2021-09-05 19:46:46 -0300

    :art: removendo o antigo arquivo knapsack
    Co-authored-by: Pedrok99 <pedrohlima20@gmail.com>

--- Backend/main.py
+++ Backend/main.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 from Views.charge_view import charge_view
 
-# from knapsack import Charge, bubbleSort_charge, knapsack
-
 app = Flask(__name__)
 app.register_blueprint(charge_view)
 CORS(app)
